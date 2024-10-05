902faa66bd0f88006295b4c3d272dbea9f9cd20e
notjuliet <notjuliet@riseup.net>
2024-10-05 11:46:43 +0200

    remove save char

--- src/components/PostComposer.tsx
+++ src/components/PostComposer.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal, onMount, Setter } from "solid-js";
+import { Component, Setter } from "solid-js";
 import { isLoggedIn, loginState } from "./Login.jsx";
 import { SocialPskyFeedPost } from "@atcute/client/lexicons";
 import * as TID from "@atcute/tid";
@@ -7,25 +7,8 @@ import { APP_NAME, CHARLIMIT, SERVER_URL } from "../utils/constants.js";
 const PostComposer: Component<{ setUnreadCount: Setter<number> }> = ({
   setUnreadCount,
 }) => {
-  const [saveToggle, setSaveToggle] = createSignal(false);
-  const [firstChar, setFirstChar] = createSignal("");
   let postInput = "";
   const segmenter = new Intl.Segmenter();
-
-  onMount(() => {
-    setSaveToggle(
-      localStorage.getItem("saveFirstChar") === "true" ? true : false,
-    );
-
-    setFirstChar(localStorage.getItem("firstChar") ?? "");
-    const textInputElem = document.getElementById(
-      "textInput",
-    ) as HTMLInputElement;
-    if (saveToggle() && firstChar()) {
-      textInputElem.setAttribute("value", firstChar());
-      textInputElem.value = firstChar();
-    }
-  });
 
   const graphemeLen = (text: string): number => {
     let iterator = segmenter.segment(text)[Symbol.iterator]();
@@ -65,22 +48,6 @@ const PostComposer: Component<{ setUnreadCount: Setter<number> }> = ({
 
   return (
     <div class="mb-4 flex items-center justify-center">
-      <input
-        type="checkbox"
-        id="saveChar"
-        checked={saveToggle()}
-        class="mr-2 accent-stone-600"
-        onChange={(e) => {
-          setSaveToggle(e.currentTarget.checked);
-          localStorage.setItem(
-            "saveFirstChar",
-            saveToggle() ? "true" : "false",
-          );
-        }}
-      />
-      <label for="saveChar" class="w-10 text-xs">
-        save char
-      </label>
       <form
         id="postForm"
         onsubmit={(e) => {
@@ -94,7 +61,7 @@ const PostComposer: Component<{ setUnreadCount: Setter<number> }> = ({
           placeholder="64 chars max"
           required
           autocomplete="off"
-          class="mr-2 w-52 border border-black px-2 py-1 font-sans sm:w-64 dark:border-white dark:bg-neutral-700"
+          class="mr-2 w-52 border border-black px-2 py-1 font-sans dark:border-white dark:bg-neutral-700 sm:w-64"
           onInput={(e) => (postInput = e.currentTarget.value)}
           onPaste={(e) => {
             if (
@@ -111,16 +78,7 @@ const PostComposer: Component<{ setUnreadCount: Setter<number> }> = ({
           onclick={() => {
             if (!postInput.length) return;
             sendPost(postInput);
-            if (saveToggle()) {
-              setFirstChar([...segmenter.segment(postInput)][0].segment);
-              const textInputElem = document.getElementById(
-                "textInput",
-              ) as HTMLInputElement;
-              textInputElem.setAttribute("value", firstChar());
-              textInputElem.value = firstChar();
-              localStorage.setItem("firstChar", firstChar());
-            }
-            postInput = saveToggle() ? firstChar() : "";
+            postInput = "";
           }}
           class="bg-stone-600 px-1 py-1 text-sm font-bold text-white hover:bg-stone-700"
         >
