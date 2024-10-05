2339eef3d7569a9eec62fcbb775c82d1547aaf54
notjuliet <notjuliet@riseup.net>
2024-10-05 11:40:09 +0200

    change style

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -13,13 +13,11 @@ const PostItem: Component<PostItemProps> = (props: PostItemProps) => {
       <div class="my-0.5 flex max-h-40 w-full flex-col items-start">
         <span class="flex w-full items-center justify-between gap-x-2 break-words text-xs text-stone-500 dark:text-stone-400">
           <span>
-            {props.record.nickname ? `${props.record.nickname} ` : ""}
+            <span class="text-violet-600 dark:text-violet-400">
+              {props.record.nickname ? `${props.record.nickname} ` : ""}
+            </span>
             <a
-              classList={{
-                "text-violet-600 dark:text-violet-400":
-                  props.record.handle !== "anon.psky.social",
-                truncate: true,
-              }}
+              class="truncate"
               target="_blank"
               href={`https://bsky.app/profile/${props.record.handle}`}
             >
