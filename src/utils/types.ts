a08670b35e3e7951f07c20d3ad739940c67480c8
Elaina <48662592+oestradiol@users.noreply.github.com>
2024-10-06 02:14:59 -0300

    Updating lexicon

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,10 @@
+import { Facet } from "./rich-text/defs.js";
+
 interface PostRecord {
   did: string;
   rkey: string;
   post: string;
+  facets: Facet[];
   handle: string;
   nickname?: string;
   indexedAt: number;
