dd48b6aa19db2a5cde5f69a89de92e08e609dc7a
notjuliet <notjuliet@riseup.net>
2024-10-08 05:56:41 +0200

    fix nicknames hiding handles if too long

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -45,16 +45,16 @@ const PostItem: Component<PostItemProps> = (props: PostItemProps) => {
         }}
       >
         <span class="flex w-full items-center justify-between gap-x-2 break-words text-xs text-stone-500 sm:text-sm dark:text-stone-400">
-          <span class="w-full truncate">
+          <span class="flex w-full min-w-0 grow">
             <a
-              class="font-bold text-black dark:text-white"
+              class="truncate font-bold text-black dark:text-white"
               target="_blank"
               href={`https://bsky.app/profile/${props.record.handle}`}
             >
               {props.record.nickname ? `${props.record.nickname} ` : ""}
             </a>
             <span
-              class="cursor-pointer text-zinc-600 dark:text-zinc-400"
+              class="w-fit max-w-full shrink-0 cursor-pointer truncate text-zinc-600 dark:text-zinc-400"
               onclick={() => {
                 const newInput = `${postInput()}@${props.record.handle} `;
                 if (graphemeLen(newInput) > CHARLIMIT) return;
@@ -70,7 +70,7 @@ const PostItem: Component<PostItemProps> = (props: PostItemProps) => {
             </span>
           </span>
 
-          <span class="w-32 text-right font-mono text-xs">
+          <span class="w-fit shrink-0 text-right font-mono text-xs">
             {new Date(props.record.indexedAt).toLocaleTimeString([], {
               hour: "2-digit",
               minute: "2-digit",
