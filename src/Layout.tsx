504e40e39e15a59d3a08bc40c7159706b2b7ae64
Elaina <48662592+oestradiol@users.noreply.github.com>
2024-10-10 23:03:20 -0300

    Showing scrollbar

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -23,7 +23,7 @@ const Layout: Component = () => {
         <Header />
         <Login />
       </div>
-      <div class="hide-scroll flex min-h-0 flex-1 justify-center overflow-auto px-2">
+      <div class="flex min-h-0 flex-1 justify-center overflow-auto px-5">
         <PostFeed />
       </div>
       <Show when={isLoggedIn()}>
